72186ea50a9cfb95a6a78a35b947fb4f88da8cfc
Vinícius <ozirispc@gmail.com>
2020-01-26 19:52:03 -0300

    First commit without redux

---
+++ src/components/TransactionForm.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class TransactionForm extends Component {
+  state = {
+    ...this.returnStateObject()
+  };
+
+  returnStateObject() {
+    if (this.props.currentIndex === -1)
+      return {
+        bAccountNo: '',
+        iFSC: '',
+        bName: '',
+        amount: ''
+      };
+    else return this.props.list[this.props.currentIndex];
+  }
+
+  componentDidUpdate(prevProps) {      
+    if (
+      prevProps.currentIndex !== this.props.currentIndex ||
+      prevProps.list.length !== this.props.list.length
+    )
+      this.setState({
+        ...this.returnStateObject()
+      });
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onAddOrEdit(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} autoComplete='off'>
+        <input
+          name='bAccountNo'
+          value={this.state.bAccountNo}
+          placeholder='inform bAccountNo'
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <input
+          name='iFSC'
+          value={this.state.iFSC}
+          placeholder='inform iFSC'
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <input
+          name='bName'
+          value={this.state.bName}
+          placeholder='inform bName'
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <input
+          name='amount'
+          value={this.state.amount}
+          placeholder='inform amount'
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default TransactionForm;
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import TransactionForm from "./TransactionForm";
+
+class TransactionList extends Component {
+  state = {
+    currentIndex: -1,
+    list: this.returnList()
+  };
+
+  returnList() {
+    if (localStorage.getItem("transactions") == null)
+      localStorage.setItem("transactions", JSON.stringify([]));
+
+    return JSON.parse(localStorage.getItem("transactions"));
+  }
+
+  onAddOrEdit = data => {
+    var list = this.returnList();
+
+    if (this.state.currentIndex === -1) list.push(data);
+    else list[this.state.currentIndex] = data;
+
+    localStorage.setItem("transactions", JSON.stringify(list));
+    this.setState({ list, currentIndex: -1 });
+  };
+
+  handleEdit = index => {
+    this.setState({
+      currentIndex: index
+    });
+  };
+
+  handleDelete = index => {
+    var list = this.returnList();
+    list.splice(index, 1);
+    localStorage.setItem("transactions", JSON.stringify(list));
+    this.setState({ list, currentIndex: -1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <TransactionForm
+          onAddOrEdit={this.onAddOrEdit}
+          currentIndex={this.state.currentIndex}
+          list={this.state.list}
+        />
+        <hr />
+        <table>
+          <tbody>
+            {this.state.list.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.bAccountNo}</td>
+                  <td>{item.iFSC}</td>
+                  <td>{item.bName}</td>
+                  <td>{item.amount}</td>
+                  <td>
+                    <button onClick={() => this.handleEdit(index)}>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick={() => this.handleDelete(index)}>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default TransactionList;
